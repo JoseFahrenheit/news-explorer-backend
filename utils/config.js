d5db208b018c3127dcfe2a1f5265fc5a2542ec34
fahren <jose.ssj424@gmail.com>
2025-11-10 21:03:46 -0600

    Actualizacion para despliegue

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,9 @@
-const { JWT_SECRET = 'secret-key-development' } = process.env;
-const { NODE_ENV = 'development' } = process.env;
-const { PORT = 3000 } = process.env;
-const { DB_URL = 'mongodb://localhost:27017/news-explorer' } = process.env;
+const {
+  JWT_SECRET = 'secret-key-development',
+  NODE_ENV = 'development',
+  PORT = 3000,
+  DB_URL = 'mongodb://localhost:27017/news-explorer'
+} = process.env;
 
 module.exports = {
   JWT_SECRET,
